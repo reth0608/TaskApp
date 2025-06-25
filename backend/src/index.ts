a2b12c5afa6cd7c3a6783e3f84fb441689e26c8b
Rethash <rethashdevreddy@gmail.com>
2025-06-25 23:25:05 +0530

    Port removed from main file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,8 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (_, res) => res.send("Kaam Karjaaaaa"));
+app.get("/", (_, res) => res.send("Backend Works"));
 app.use("/api/tasks", tasksRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
